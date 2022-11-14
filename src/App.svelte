beeeab3f5d18b08298fbd3e1d489d1f2632f5f66
alanmunozlopez <alanbd@worksitelabs.com>
2022-11-14 13:19:04 -0300

    am - first commid

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+	export let name;
+	export let lastName;
+  let svelteLogo = "https://arepa.s3.amazonaws.com/svelte-logo.png";
+</script>
+
+<main>
+	<h1>Hello {name} {lastName}!</h1>
+  <p> Frotend Developer </p>
+  <img src={svelteLogo} alt="Svelte" />
+</main>
+
+<style>
+  /* global styles */
+  :global(body) {
+    background-color: #f2eee2;
+    color: #0085f6;
+  }
+
+  :global(:root) {
+    --theme-color: purple;
+  }
+
+  p {
+    color: var(--theme-color);
+    font-size: 22px;
+  }
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+  main img {
+    width: 30%;
+  }
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
